ae13cd093fcd57de9d661864cbac12044391e120
Tomas Mayr <tmayr@tmayr.com>
2015-03-10 12:50:19 -0300

    removed link, won't implement for now

--- src/views/Header.jsx
+++ src/views/Header.jsx
@@ -6,7 +6,6 @@ export default React.createClass({
             <header>
                 <section className="header-logo"></section>
                 <nav className="header-nav">
-                    <a href="">Latest Additions</a>
                     <a href="">My Location</a>
                 </nav>
             </header>
